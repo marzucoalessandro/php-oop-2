ae3895544c1e180c79aef9857c8967f9f90117db
Alessandro <marzucoalessandro1@gmail.com>
2021-02-16 16:25:12 +0100

    prima parte esercizio

---
+++ classes/user.php
@@ -0,0 +1,18 @@
+<?php
+class User{
+  public $user_name;
+  public $user_lastName;
+  public $user_id;
+  public $user_password;
+  public $user_age_of_birth;
+
+  public function __construct(string $user_name, string $user_lastName, string $user_id, string $user_password, string $user_age_of_birth){
+
+    $this->user_name = $user_name;
+    $this->user_lastName = $user_lastName;
+    $this->user_id = $user_id;
+    $this->user_password = $user_password;
+    $this->user_age_of_birth = $user_age_of_birth;
+  }
+}
+ ?>
---
+++ index.php
@@ -0,0 +1,44 @@
+<!-- Istruzioni:
+Crea un diagramma per una tabella di db che rappresenti gli Users di un blog.
+Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users.
+Il database e la tabella non devono essere realmente creati.
+Bonus (non tanto facoltativo):
+Una volta finita la classe User, pensate a che altre entitá possiamo avere in un sistema di Blogging oltre all'utente.
+Per darvi un idea, un blog ha degli articoli, categorie, tags etc.
+Create nel diagramma anche le altre entitá e definite per ciascuna le rispettive classi con proprietá e metodi. -->
+
+<?php
+include __DIR__ . "./classes/user.php";
+
+$user1 = new User("Angelo", "Dotti", "angelo83", "password1", "24-05-1983");
+$user2 = new User("Mario", "Bianchi", "white_M", "password2", "10-01-1998");
+$user3 = new User("Federica", "Carli", "fedefede", "password3", "15-09-1994");
+$user4 = new User("Bruno", "Brunetti", "bruno123456789", "password4", "03-02-1988");
+$user5 = new User("Paolo", "Fiore", "Fior_Paolo", "password", "12-08-1973");
+$user6 = new User("Aldo", "Biscardi", "Aldone50", "password", "20-07-1954");
+// var_dump($user6);
+$usersList = [$user1, $user2, $user3, $user4, $user5, $user6];
+// var_dump($usersList);
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Lista Utenti</h1>
+    <?php foreach ($usersList as $value): ?>
+      <div>
+        <h2><?php echo "$value->user_name $value->user_lastName" ?></h2>
+        <h3><?php echo ($value->user_id); ?></h3>
+        <h4><?php echo ($value->user_age_of_birth); ?></h4>
+        <br>
+
+      </div>
+    <?php endforeach; ?>
+  </body>
+</html>
